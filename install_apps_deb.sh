58af885267118762ddf274cddbb33b746162e5b9
Erica Zavaleta <44880722+EntropySynthetica@users.noreply.github.com>
2020-05-20 18:13:57 +0000

    Updated Deb dot file deploy script

--- install_apps_deb.sh
+++ install_apps_deb.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
 cd ~
-apt-get install lolcat vim tmux vim-nox git python-pip fontconfig -y
-sudo pip install git+git://github.com/Lokaltog/powerline
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-sudo mv PowerlineSymbols.otf /usr/share/fonts/
-sudo fc-cache -vf
-sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+
+# Install Apt Packages I want
+sudo apt install git vim tmux screenfetch powerline -y
+
+### Setup steps for VIM ###
+# Install VIM plugin manager
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install Airline for VIM
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+# Install Airline Themes for VIM
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
+# Install Color Scheme
+mkdir -p ~/.vim/colors
+wget -O ~/.vim/colors/wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+
+mv ~/.bashrc ~/.bashrc.old
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+
+mv ~/.vimrc ~/.vimrc.old
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+mv ~/.tmux.conf ~/.tmux.conf.old
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+mv ~/.gitconfig ~/.gitconfig.old
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
